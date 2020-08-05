608573ce9fda492370c5116cb1301f6fc8d1d133
Jimd3v <jeremy.gth.dev@gmail.com>
2020-08-05 23:38:31 +0200

    Add draft front

---
+++ front/src/components/pages/About.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>A propos</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, temporibus commodi iste quaerat alias similique nobis beatae labore aut sunt repellat neque impedit, quidem quas voluptates blanditiis tempore nostrum a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, natus ab. A, voluptatibus illum officia adipisci aut nihil commodi saepe rem impedit, dolorem, obcaecati necessitatibus voluptate quas fugit voluptatem excepturi.</p>
+        <p>
+            <router-link to="/">
+            Retour à l'accueil
+            </router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
